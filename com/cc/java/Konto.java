fb3e195e02c66bc08a21c15e35917d43ec00d121
Arian Hormozfar <a.hormozfar@yahoo.de>
2024-07-16 17:54:53 +0200

    Zweite Variante: Setter/Getter

--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -2,6 +2,16 @@ package com.cc.java;
 
 public class Konto{
 
-    public int kontostand = 10000;
+    private int kontostand = 10000;
+
+    // Setter
+    public int getKontostand() {
+        return kontostand;
+    }
+    // Getter
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+    
     
 }
